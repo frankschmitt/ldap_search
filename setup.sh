f365996d0d16f102a1ee3df034f1b875bde5e34b
Mike <m8r0wn@protonmail.com>
2018-11-07 16:30:24 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@
 # Description: ldap_search setup script, installs Impacket for python36 from github
 
 echo -e "[*] Installing Impacket library for Python 3.6"
+# load submodule
+git submodule init
+git submodule update
+
 # Temp solution until Impacket v.0.9.18 released with Python3 support - I know it's messy
 cd impacket
 python3 setup.py install
